856d819bdf87bdd96b2c1284ecc650333bee1e90
kevalbhaivadgasiya125530-lang <kevalbhai.vadgasiya125530@marwadiuniversity.ac.in>
2026-08-06 08:30:07 +0530

    Add files via upload

---
+++ add.php
@@ -0,0 +1,156 @@
+None selected
+
+Skip to content
+Using Marwadi Education Foundation Group of Institutes Mail with screen readers
+
+1 of 857
+Fwd:
+Inbox
+
+RAMDEVSINH JADEJA
+Attachments
+8:19 AM (1 minute ago)
+to me, NIKUNJ, DHARMIK
+
+
+
+---------- Forwarded message ---------
+From: DISHANK PARSANA <dishank.parsana126283@marwadiuniversity.ac.in>
+Date: Thu, Aug 6, 2026 at 8:16 AM
+Subject:
+To: RAMDEVSINH JADEJA <ramdevsinh.jadeja124376@marwadiuniversity.ac.in>
+
+
+image.png
+
+
+ 4 Attachments
+  •  Scanned by Gmail
+<?php
+include("db.php");
+
+$username = "";
+$password = "";
+$cpassword = "";
+$phone = "";
+$email = "";
+
+if(isset($_POST['save']))
+{
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $cpassword = $_POST['cpassword'];
+    $phone = $_POST['phone'];
+    $email = $_POST['email'];
+
+    if($password != $cpassword)
+    {
+        echo "<script>alert('Password and Confirm Password must be same');</script>";
+    }
+    else
+    {
+        $photo = $_FILES['photo']['name'];
+
+        move_uploaded_file(
+            $_FILES['photo']['tmp_name'],
+            "images/".$photo
+        );
+
+        $sql = "INSERT INTO users
+        (username,password,cpassword,phone,email,photo)
+        VALUES
+        ('$username','$password','$cpassword','$phone','$email','$photo')";
+
+        mysqli_query($conn,$sql);
+
+        echo "<script>alert('Record Saved Successfully');</script>";
+
+        $username = "";
+        $password = "";
+        $cpassword = "";
+        $phone = "";
+        $email = "";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Add User</title>
+</head>
+<body>
+
+<h2>Add User</h2>
+
+<form method="post" enctype="multipart/form-data">
+
+User Name :
+<input
+type="text"
+name="username"
+value="<?php echo $username; ?>"
+required>
+
+<br><br>
+
+Password :
+<input
+type="password"
+name="password"
+value="<?php echo $password; ?>"
+required>
+
+<br><br>
+
+Confirm Password :
+<input
+type="password"
+name="cpassword"
+value="<?php echo $cpassword; ?>"
+required>
+
+<br><br>
+
+Phone :
+<input
+type="text"
+name="phone"
+value="<?php echo $phone; ?>">
+
+<br><br>
+
+Email :
+<input
+type="email"
+name="email"
+value="<?php echo $email; ?>">
+
+<br><br>
+
+Photo :
+<input
+type="file"
+name="photo">
+
+<br><br>
+
+<input
+type="submit"
+name="save"
+value="Save">
+
+<input
+type="reset"
+value="Clear">
+
+</form>
+
+<br>
+
+<a href="display.php">View Records</a>
+
+</body>
+</html>
+add.php
+Displaying add.php.
